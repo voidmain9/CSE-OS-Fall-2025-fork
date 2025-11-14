c518b20e0b745edac7238b08dc023f3f0ea8301f
Daniel B <danbok2016@outlook.com>
2025-11-13 21:08:54 -0500

    removed unused enum

--- CSE4300/abhinav-daniel-eric-vin/schedule-main.h
+++ CSE4300/abhinav-daniel-eric-vin/schedule-main.h
@@ -1,13 +1,6 @@
 #ifndef SCHEDULE_MAIN_H
 #define SCHEDILE_MAIN_H
 #include "process.h"
-enum QueueTypes {
-    ROUNDROBIN, //priority 1
-    STCF,       //priority 2
-    FIFO,       //priority 3
-    SJF         //priority 4
-};
-
 
 void scheduleMain(struct process **procArray, int procArraySize, int maxTimesteps);
 
