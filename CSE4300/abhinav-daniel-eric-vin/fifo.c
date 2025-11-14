06a7f0f9b81ebdf36ca9b03415718ef1b4193e38
Daniel B <danbok2016@outlook.com>
2025-11-13 20:48:16 -0500

    added comments

--- CSE4300/abhinav-daniel-eric-vin/fifo.c
+++ CSE4300/abhinav-daniel-eric-vin/fifo.c
@@ -7,13 +7,14 @@ void fifo(struct process **FIFOQueue, int* queueptr, int globalTime){
     //rearrange for this algorithm
     //(no rearranging needed for fifo)
     //subtract from procArray[0]'s remaining time ("executing the process")
+
+    //if remainingtime == 0:
+    //set finishtime
+    //remove procArray[0] from queue
+    //shift all other processes in the queue down so the next one is in procArray[0]
+    //update queueptr
     printf("Time %d: Executing %s   FIFO\n", globalTime, FIFOQueue[0]->name);
     FIFOQueue[0]->remainingTime--;
-    //if remainingtime == 0:
-        //set finishtime
-        //remove procArray[0] from queue
-        //shift all other processes in the queue down so the next one is in procArray[0]
-        //update queueptr
 
     if(FIFOQueue[0]->remainingTime == 0) {
         FIFOQueue[0]->finishTime = globalTime+1;
